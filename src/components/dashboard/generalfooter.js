f88975238f5c199411a96c0c981fb11a7d147031
anmolkoirala <anmolkoirala3@gmail.com>
2020-01-12 21:38:02 +0545

    added footer file for general user

---
+++ src/components/dashboard/generalfooter.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class GeneralFooter extends Component{
+    render(){
+        return(
+            <footer className="footer">
+             <div className="d-sm-flex justify-content-center justify-content-sm-between">
+                 <span className="text-muted text-center text-sm-left d-block d-sm-inline-block">
+                     Copyright © 2020 Grocery Store Shopping.</span>
+                 <span className="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">
+                     User Panel | React Basics</span>
+             </div>
+             </footer>
+        );
+    }
+}
+
+export default GeneralFooter;
